96037b3d9ca4d08b491f5c07b181423ba0797ba1
chrissyakhara <chrissya.kharra.1903126@students.um.ac.id>
2022-10-24 11:14:29 +0700

    belahar php1

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+echo "hello world<br>";
+
+$nama = "Chrissya";
+$umur = 21;
+
+echo "nama saya $nama, saya berumur $umur tahun.<br>";
+
+$namaAdik = "sasa";
+$umurAdik = 10;
+echo "nama adik saya $nama, adik saya berumur $umurAdik tahun<br>";
+
+$selisih = $umur-$umurAdik ;
+
+echo "selisih umur saya dan adik saya $selisih.";
+
+?>
